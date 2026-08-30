b26b96c3ea7fc3190f780f745e50f2a456043ad1
iroha <y-kobayashi@fusic.co.jp>
2026-08-30 22:43:06 +0900

    remove: 不要なテスト用スキーマを削除 #1

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,19 +1,12 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const todos = sqliteTable("todos", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  title: text("title").notNull(),
-  done: integer("done", { mode: "boolean" }).notNull().default(false),
-});
-
-export type Todo = typeof todos.$inferSelect;
+import { task } from "../messages";
 
 export const tasks = sqliteTable("tasks", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name", { length: 30 }).notNull(),
   startAt: text("start_at"),
-  dueAt: text("due_at").notNull(),
-  status: text("status", { length: 10 }).notNull(),
+  dueAt: text("due_at"),
+  status: text("status", { length: 10 }).notNull().default(task.plan),
   detail: text("detail", { length: 200 }),
   isDone: integer("is_done", { mode: "boolean" }).default(false),
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
-import Image from "next/image";
-import { listTodos } from "./todos";
-import { addTodo } from "./actions";
+import { getActiveTasks } from "./services/taskService";
 
 export default async function Home() {
-  const todos = await listTodos();
+  const tasks = await getActiveTasks();
 
   return (
-    <div>
-      <p>フォーム</p>
-      <form action={addTodo}>
-        <input type="text" name="title" />
-        <button type="submit">追加</button>
-      </form>
-
-      <p>リスト一覧</p>
-      <ul>
-        {todos.map((todo) => (
-          <li>
-            {todo.id} {todo.title}
+    <div className="container py-4">
+      <h2 className="h5 mt-4">リスト一覧</h2>
+      <ul className="list-group">
+        {tasks.map((todo) => (
+          <li key={todo.id} className="list-group-item">
+            {todo.id} {todo.name}
           </li>
         ))}
       </ul>
