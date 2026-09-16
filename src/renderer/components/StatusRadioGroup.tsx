c8b591eb80f8c0f84bc7b516f0ad4c8592198cb6
iroha <y-kobayashi@fusic.co.jp>
2026-09-17 00:10:55 +0900

    feat: 状態の入力をラジオボタンに変更 #16

--- src/renderer/components/StatusRadioGroup.tsx
+++ src/renderer/components/StatusRadioGroup.tsx
@@ -1,65 +1,76 @@
-import { Pencil } from "lucide-react";
+import {
+  ArrowRight,
+  Construction,
+  MessageCircleCode,
+  Package,
+  Pencil,
+} from "lucide-react";
 import { Field, FieldContent, FieldLabel, FieldTitle } from "./ui/field";
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group";
 
 export default function StatusRadioGroup() {
   return (
-    <ToggleGroup type="single" variant="outline" defaultValue="plan">
-        <ToggleGroupItem value="plan" aria-label="予定" className="size-16 flex flex-col items-center justify-center">
-          <span className="text-2xl leading-none font-light">
-            <Pencil />
-          </span>
-          <span className="text-xs text-muted-foreground">予定</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem value="thisweek" aria-label="予定" className="size-16 flex flex-col items-center justify-center">
-          <span className="text-2xl leading-none font-light">
-            <Pencil />
-          </span>
-          <span className="text-xs text-muted-foreground">予定</span>
-        </ToggleGroupItem>
-      </ToggleGroup>
-    // <RadioGroup defaultValue="plan" className="flex ">
-    //   <FieldLabel htmlFor="plan">
-    //     <Field orientation="horizontal">
-    //       <FieldContent>
-    //         <FieldTitle>予定</FieldTitle>
-    //       </FieldContent>
-    //       <RadioGroupItem value="plan" id="plan" />
-    //     </Field>
-    //   </FieldLabel>
-    //   <FieldLabel htmlFor="thisweek">
-    //     <Field orientation="horizontal">
-    //       <FieldContent>
-    //         <FieldTitle>今週やること</FieldTitle>
-    //       </FieldContent>
-    //       <RadioGroupItem value="thisweek" id="thisweek" />
-    //     </Field>
-    //   </FieldLabel>
-    //   <FieldLabel htmlFor="wip">
-    //     <Field orientation="horizontal">
-    //       <FieldContent>
-    //         <FieldTitle>作業中</FieldTitle>
-    //       </FieldContent>
-    //       <RadioGroupItem value="wip" id="wip" />
-    //     </Field>
-    //   </FieldLabel>
-    //   <FieldLabel htmlFor="inreview">
-    //     <Field orientation="horizontal">
-    //       <FieldContent>
-    //         <FieldTitle>レビュー中</FieldTitle>
-    //       </FieldContent>
-    //       <RadioGroupItem value="inreview" id="inreview" />
-    //     </Field>
-    //   </FieldLabel>
-    //   <FieldLabel htmlFor="delivering">
-    //     <Field orientation="horizontal">
-    //       <FieldContent>
-    //         <FieldTitle>検収中</FieldTitle>
-    //       </FieldContent>
-    //       <RadioGroupItem value="delivering" id="delivering" />
-    //     </Field>
-    //   </FieldLabel>
-    // </RadioGroup>
-  )
+    <ToggleGroup
+      type="single"
+      variant="outline"
+      defaultValue="plan"
+      className="w-full"
+    >
+      <ToggleGroupItem
+        value="plan"
+        aria-label="予定"
+        className="flex-1 size-16 flex flex-col items-center justify-center"
+      >
+        <span className="text-2xl leading-none font-light">
+          <Pencil />
+        </span>
+        <span className="text-xs text-muted-foreground">予定</span>
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="thisweek"
+        aria-label="今週やること"
+        className="flex-1 size-16 flex flex-col items-center justify-center"
+      >
+        <span className="text-2xl leading-none font-light">
+          <ArrowRight />
+        </span>
+        <span className="text-xs text-muted-foreground">
+          今週
+          <br />
+          やること
+        </span>
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="wip"
+        aria-label="作業中"
+        className="flex-1 size-16 flex flex-col items-center justify-center"
+      >
+        <span className="text-2xl leading-none font-light">
+          <Construction />
+        </span>
+        <span className="text-xs text-muted-foreground">作業中</span>
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="inreview"
+        aria-label="レビュー中"
+        className="flex-1 size-16 flex flex-col items-center justify-center"
+      >
+        <span className="text-2xl leading-none font-light">
+          <MessageCircleCode />
+        </span>
+        <span className="text-xs text-muted-foreground">レビュー中</span>
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="delivering"
+        aria-label="検収中"
+        className="flex-1 size-16 flex flex-col items-center justify-center"
+      >
+        <span className="text-2xl leading-none font-light">
+          <Package />
+        </span>
+        <span className="text-xs text-muted-foreground">検収中</span>
+      </ToggleGroupItem>
+    </ToggleGroup>
+  );
 }
